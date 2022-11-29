0522cc2cff04ac4c9ff80d917022d4de2bf0db86
Fernando <fernandoms1078@hotmail.com>
2022-11-29 15:46:56 +0100

    Gracias a la ayuda de Dani soluciono lo de mostrar el nombre de la categoría de gastos

--- src/components/ViewExpensesModal.js
+++ src/components/ViewExpensesModal.js
@@ -4,16 +4,17 @@ import { useBudgets } from '../contexts/BudgetContexts'
 
 export default function ViewExpensesModal({show, handleClose, budgetId}) {
     const { budgets } = useBudgets()
-    const  budgetNames = budgets.filter(budget => budget.id === budgetId)
-    
+    const budgetNames = budgets.filter(budget => budget.id === budgetId)
+    const expensesName = budgetNames.map(budget => budget.name)
+
     console.log(budgetNames);
   return ( 
     <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-            Gastos {budgetNames[0].name}
+          {expensesName}
         </Modal.Header>
         <Modal.Body>
-            Un texto cualquiera
+          Un texto cualquiera
         </Modal.Body>
     </Modal>
   )
